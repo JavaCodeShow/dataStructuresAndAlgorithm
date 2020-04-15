9bf92d6bcdcd0c39b95b1c8bfe4f7f4b04bb868e
江峰 <jiangfeng@smarchit.com>
2020-04-15 11:12:48 +0800

    插入排序

--- src/algorithm/sort/InsertSort.java
+++ src/algorithm/sort/InsertSort.java
@@ -3,6 +3,14 @@ package algorithm.sort;
 import java.util.Arrays;
 
 /**
+ * 插入排序
+ * ①. 从第一个元素开始，该元素可以认为已经被排序
+ * ②. 取出下一个元素，在已经排序的元素序列中从后向前扫描
+ * ③. 如果该元素（已排序）大于新元素，将该元素移到下一位置
+ * ④. 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+ * ⑤. 将新元素插入到该位置后
+ * ⑥. 重复步骤②~⑤
+ *
  * @author 潇潇暮雨
  * @create 2018-09-07   15:18
  * <p>
@@ -11,7 +19,7 @@ import java.util.Arrays;
 public class InsertSort {
 
     public void insertSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
                 swap(arr, j, j - 1);
             }
@@ -26,8 +34,7 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 3, 2, 5, 6, 8, 7};
-        InsertSort sort = new InsertSort();
-        sort.insertSort(arr);
+        new InsertSort().insertSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
